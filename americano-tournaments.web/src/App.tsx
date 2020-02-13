48e927d8a1e77ddf88b62e4ea64833e229cf443f
Olli Hanhimäki <ohanhimaki@gmail.com>
2020-02-13 21:36:07 +0200

    typescripti

---
+++ americano-tournaments.web/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Newtournament } from "./new-tournament/Newtournament";
+
+function App() {
+  return <Newtournament></Newtournament>;
+}
+
+export default App;
