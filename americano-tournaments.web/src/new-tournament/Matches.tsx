c892ca8789ee398cee49689fead9e5cff6978259
Olli Hanhimäki <ohanhimaki@gmail.com>
2020-02-23 10:56:53 +0200

    Ui toiminnallisuutta kehitetty matseihin

--- americano-tournaments.web/src/new-tournament/Matches.tsx
+++ americano-tournaments.web/src/new-tournament/Matches.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import tournamentState from "./models/tournamentState";
-import Match, { Team } from "./models/tournament";
+import Match, { Team, Player } from "./models/tournament";
 
 export const Matches = () => {
   const [updated, setupdated] = useState(new Date());
+  const [highlightedPlayer, sethighlightedPlayer] = useState("");
+
   const tournamentinst = tournamentState.getInstance();
 
   function addScore(match: Match, team: Team, sumToAdd: number) {
@@ -16,6 +18,41 @@ export const Matches = () => {
 
     setupdated(new Date());
   }
+
+  function highlightPlayer(player?: Player) {
+    if (!player) {
+      return;
+    }
+    sethighlightedPlayer(player.name);
+  }
+
+  function getPlayer(player?: Player) {
+    let classstring = "bg-blue-300";
+    if (!player) {
+      return;
+    }
+
+    if (player.name === highlightedPlayer) {
+      classstring = "bg-red-300";
+    }
+    classstring += " py-2 px-4";
+    return (
+      <div className={classstring}>
+        <button onClick={() => highlightPlayer(player)}>{player.name}</button>
+      </div>
+    );
+  }
+
+  function getMatchStateString(state?: number) {
+    if (state === 0) {
+      return "upcoming";
+    } else if (state === 1) {
+      return "ongoing";
+    } else if (state === 2) {
+      return "finished";
+    }
+  }
+
   return (
     <div className="">
       <table className="table-auto m-auto">
@@ -34,32 +71,33 @@ export const Matches = () => {
           {tournamentinst.matches.map((match: Match, index: number) => {
             return (
               <tr key={index}>
+                <td className="border px-4 py-2">{match.roundno}</td>
                 <td className="border px-4 py-2">{match.matchno}</td>
-                <td className="border px-4 py-2">
-                  <div>{match.team1.players[0].name}</div>
-                  <div>{match.team1.players[1].name}</div>
+                <td className="border">
+                  {getPlayer(match.team1.players[0])}
+                  {getPlayer(match.team1.players[1])}
                 </td>
                 <td
                   className="border px-4 py-2"
                   onClick={() => addScore(match, match.team1, 1)}
                 >
-                  {match.score1}
+                  <button>{match.score1}</button>
                 </td>
                 <td
                   className="border px-4 py-2"
                   onClick={() => addScore(match, match.team2, 1)}
                 >
-                  {match.score2}
+                  <button>{match.score2}</button>
                 </td>
                 <td className="border px-4 py-2">
-                  <div>{match.team2.players[0].name}</div>
-                  <div>{match.team2.players[1].name}</div>
+                  {getPlayer(match.team2.players[0])}
+                  {getPlayer(match.team2.players[1])}
                 </td>
                 <td
                   className="border px-4 py-2"
                   onClick={() => changeStatus(match)}
                 >
-                  {match.status}
+                  <button>{getMatchStateString(match.status)}</button>
                 </td>
               </tr>
             );
