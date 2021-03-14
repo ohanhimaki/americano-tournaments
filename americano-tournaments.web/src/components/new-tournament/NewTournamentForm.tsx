1d62f09746579c831c3a4874e4492e1a8c56504b
Olli Hanhimäki <ohanhimaki@gmail.com>
2021-03-14 11:35:33 +0200

    IImportatut talteen localstorageen ja tournament iimport omaan componenttiin

--- americano-tournaments.web/src/components/new-tournament/NewTournamentForm.tsx
+++ americano-tournaments.web/src/components/new-tournament/NewTournamentForm.tsx
@@ -29,15 +29,6 @@ test8`;
         selectTournament(tournamentState.getInstance());
     }
 
-    function handleImportSubmit(event: any) {
-        event.preventDefault();
-        let importJson = event.target.importJson.value;
-        console.log(importJson)
-        var tournament = JSON.parse(importJson);
-        const tournamentinst = tournamentState.getInstance();
-        tournamentinst.reloadOldTournament(tournament);
-        selectTournament(tournamentinst);
-    }
 
     return(
     <div className="m-auto w-full flex flex-row flex-wrap justify-center items-start">
@@ -90,31 +81,6 @@ test8`;
                             type="datetime-local"
                             name="startTime"
                             defaultValue={defaultDateTime}
-                            className="md:w-2/3 w-full bg-gray-800 border border-palayellow-300 p-1 text-gray-200 rounded-sm"
-                        />
-                    </div>
-                    <div className="md:flex pb-2">
-                        <div className="md:w-1/3 mr-3"/>
-                        <input
-                            type="submit"
-                            value="Submit"
-                            className="bg-palayellow-300 text-gray-900 px-2 py-1 rounded-md"
-                        />
-                    </div>
-                </form>
-                <form
-                    action=""
-                    onSubmit={handleImportSubmit}
-                    className="w-full max-w-md m-auto "
-                >
-                    <div className="md:flex pb-2">
-                        <label className="md:w-1/3 md:text-right mr-3" htmlFor="">
-                            ImportJson:
-                        </label>
-                        <input
-                            type="text"
-                            name="importJson"
-                            defaultValue={""}
                             className="md:w-2/3 w-full bg-gray-800 border border-palayellow-300 p-1 text-gray-200 rounded-sm"
                         />
                     </div>
